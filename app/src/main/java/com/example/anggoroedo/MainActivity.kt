3c1f0e32938bfb8e93c6fe2d84a2be93e01ebd22
anggoro edo <anggoroedop@gmail.com>
2021-10-05 13:15:24 +0700

    First commit

---
+++ app/src/main/java/com/example/anggoroedo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.anggoroedo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.anggoroedo.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
